d1b147a1c554c37ae9c39cac6ac3973a8bff79f7
ahmads1990 <ahmads20002000@gmail.com>
2022-12-10 23:39:36 +0200

    Jarvis March

--- CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
+++ CGAlgorithms/Algorithms/ConvexHull/JarvisMarch.cs
@@ -12,24 +12,42 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-            if (points.Count <= 3) { outPoints = points;return; }
-            //
-            Enums.TurnType turnDirection = Enums.TurnType.Right;
+            if (points.Count <= 3) { outPoints = points; return; }
             points = points.OrderBy(x => x.Y).ToList();
             int n = points.Count;
             Point a = points[0];
             Point b;
+
+            // draw line behind first point to get the second point
+            Point newp = new Point(a.X, a.Y + 10);
+            Line first_line = new Line(a, newp);
+
             outPoints.Add(a);
             //find next point
+
             double angleHolder = 0;
             int index = -1;
+
+
             for (int i = 1; i < n; i++)
             {
-                double y = Math.Abs(a.Y - points[i].Y);
-                double x = Math.Abs(a.X - points[i].X);
-                double angle = Math.Atan2(y, x);
-                if (angle > angleHolder)
+                Line test_line = new Line(a, points[i]);
+                Point vectorBA = HelperMethods.GetVector(first_line);
+                Point vectorBC = HelperMethods.GetVector(test_line);
+
+                double Numerator = vectorBA.X * vectorBC.X + vectorBA.Y * vectorBC.Y;
+                double denominator = vectorBA.Magnitude() * vectorBC.Magnitude();
+
+                double angle = Math.Acos(Numerator / denominator);
+                if (angle >= angleHolder)
                 {
+                    // cheak if the point have the same max angle value
+                    // but in the middle of the line i have aleady 
+                    // I ignore the point in this case
+                    if (angle == angleHolder)
+                    {
+                        if (HelperMethods.PointOnSegment(points[i], points[index], a)) continue;
+                    }
                     angleHolder = angle;
                     index = i;
                 }
@@ -40,29 +58,39 @@ namespace CGAlgorithms.Algorithms.ConvexHull
             //search
             for (int i = 2; i < n; i++)
             {
-                Line ba = new Line( b,a);
                 angleHolder = 0;
+                Line ba = new Line(b, a);
                 for (int c = 0; c < n; c++)
                 {
                     Line bc = new Line(b, points[c]);
+
                     Point vectorBA = HelperMethods.GetVector(ba);
                     Point vectorBC = HelperMethods.GetVector(bc);
-                    double angle = Math.Acos(
-                            (vectorBA.X * vectorBC.X + vectorBA.Y * vectorBC.Y) /
-                            vectorBA.Magnitude() * vectorBC.Magnitude()
-                        );
-                    if(angle > angleHolder)
+
+
+                    double Numerator = vectorBA.X * vectorBC.X + vectorBA.Y * vectorBC.Y;
+                    double denominator = vectorBA.Magnitude() * vectorBC.Magnitude();
+                    double angle = Math.Acos(Numerator / denominator);
+
+                    if (angle >= angleHolder)
                     {
+                        // cheak if the point have the same max angle value
+                        // but in the middle of the line i have aleady 
+                        // I ignore the point in this case
+                        if (angle == angleHolder)
+                        {
+                            if (HelperMethods.PointOnSegment(points[c], points[index], b)) continue;
+                        }
                         angleHolder = angle;
                         index = c;
                     }
                 }
                 //new point
                 a = b;
-                b=points[index];
-                outPoints.Add(b);
+                b = points[index];
+                // chcak if the point already in the outbut list or not
+                if (outPoints.Contains(b) == false) outPoints.Add(b);
             }
-            Console.WriteLine();
         }
 
         public override string ToString()
